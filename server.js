45c47c53e73ffc8f822e1c70d15714470fd4e915
Shrey0207 <shreytolasaria4297@gmail.com>
2025-05-08 18:22:11 +0530

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch'); // if using Node 18+, native fetch is available
 const cors = require('cors');
 require('dotenv').config(); // Optional: to load secrets from .env
+require('dotenv').config(); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;

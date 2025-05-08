9e9ad3f192b7a4e419369d5aa8f73b5461762988
Shrey0207 <shreytolasaria4297@gmail.com>
2025-05-08 18:27:19 +0530

    cors change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://your-qlik-mashup-domain', // Replace with your actual mashup origin
+  origin: '*', // Replace with your actual mashup origin
   credentials: true
 }));
 app.use(express.json());

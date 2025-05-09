38dd9abe4a9e3c26441e8b15bf0a1a84f5de18ff
Shrey0207 <shreytolasaria4297@gmail.com>
2025-05-09 11:17:10 +0530

    snowflake debug

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
+const snowflake = require('snowflake-sdk');
 require('dotenv').config();
 
 const app = express();
